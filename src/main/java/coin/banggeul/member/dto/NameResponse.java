11e21c6a3e51f3457df2c612c55397e0ad9c4bc0
NamJihyun99 <namjihyun9936@gmail.com>
2023-08-01 19:56:23 +0900

    feat:>get user's name

---
+++ src/main/java/coin/banggeul/member/dto/NameResponse.java
@@ -0,0 +1,14 @@
+package coin.banggeul.member.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NameResponse {
+
+    private String name;
+}
